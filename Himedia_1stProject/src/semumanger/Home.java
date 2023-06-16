8b9ff579ead8caa2f34c4bc972cf9f494c4633da
Class01 <Class01@DESKTOP-1KRANK2>
2023-06-16 17:54:57 +0900

    메인화면 소스

---
+++ Himedia_1stProject/src/semumanger/Home.java
@@ -0,0 +1,18 @@
+package semumanger;
+
+import javax.swing.JFrame;
+
+public class Home {
+	private JFrame home;
+	
+	public Home() {
+		home = new JFrame("세무매니저-홈");
+		home.setSize(400, 400);
+		home.setLayout(null);
+		
+		
+		
+		
+		home.setVisible(true);
+	}
+}
