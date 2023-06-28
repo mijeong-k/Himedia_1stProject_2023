44e6cb3d7e04fe0bc07ea1373a701d281110d837
Class01 <Class01@DESKTOP-1KRANK2>
2023-06-28 18:09:59 +0900

    1st_Project_230628_실행코드 수정

--- Himedia_1stProject/src/semumanger/Act.java
+++ Himedia_1stProject/src/semumanger/Act.java
@@ -2,10 +2,24 @@ package semumanger;
 
 public class Act {
 	
-	
 	public static void main(String[] args) {
 		Login log = new Login();
 //		Join jo = new Join();
+//		Home ho = new Home("");
+//		Request req = new Request("");
+//		Documents doc = new Documents("");
+//		FileChooserTest fileChooser = new FileChooserTest();
+//		Search sc = new Search("test@naver.com");
+//		SearchDoc sd = new SearchDoc("sj@naver.com");
+//		MngLogin mnglog = new MngLogin();
+//		MngHome mnghome = new MngHome("10001");
+//		MngSelect mngselect = new MngSelect("","1001");
+//		MngSearch mss = new MngSearch("1001");
+//		MngWorking mww = new MngWorking("10001");
+//		MngSearchDoc msd = new MngSearchDoc("1002");
+//		Idfind idf = new Idfind();
+//		Pwdfind pf = new Pwdfind();
+
 	}
 
 }
